bdb01cbb8dfb07359a02d8fa75da2f242c6ad17d
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2022-09-22 16:44:38 +0200

    Update ping.js

--- cmd/ping.js
+++ cmd/ping.js
@@ -2,8 +2,21 @@ const DiscordJS = require("discord.js");
 module.exports = {
 	data: new DiscordJS.SlashCommandBuilder()
 	.setName("ping")
-	.setDescription("Pong!"),
+	.setDescription("Replies with 'Pong'!"),
 	async execute(interaction) {
-		await interaction.reply("Pong!");
+		const replyList = [
+			"/pong",
+			"Pong",
+			"Pong!",
+			"`pong`",
+			"Well-hidden: ||pong||",
+			"**PONG**",
+			"*cricket noise*",
+			"Pong dong chu ha!",
+			"Ping!",
+			"So now what do I need to say?",
+			"**Bot status**\n:green_circle: Online\nඞ Ready for some Among Us"
+		];
+		await interaction.reply(replyList[Math.floor(Math.random() * replyList.length)]);
 	}
 };
